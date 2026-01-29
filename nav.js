484159cc6fa4690222036f3a8e383274e5ff37e6
쿼터파이 <ymsnack@naver.com>
2026-01-29 09:48:38 +0900

    nav.js에 푸터 불러오는 기능 추가

--- nav.js
+++ nav.js
@@ -4,3 +4,12 @@ fetch('/header.html')
 		document.getElementById('header').outerHTML = data;
 	})
 	.catch(error => console.error('머릿말 로드 실패:', error));
+
+fetch('/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        const footerElement = document.getElementById('footer');
+        if (footerElement) {
+            footerElement.innerHTML = data;
+        }
+    });
