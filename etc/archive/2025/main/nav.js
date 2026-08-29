f600f32ade5d4a8071732a9683b15148abf999db
gitkimcl <cnsh31_1307@cnsh.hs.kr>
2026-08-29 15:31:25 +0900

    archiving

--- etc/archive/2025/main/nav.js
+++ etc/archive/2025/main/nav.js
@@ -1,11 +1,11 @@
-fetch('/header.html')
+fetch('/etc/archive/2025/main/header.html')
 	.then(response => response.text())
 	.then(data => {
 		document.getElementById('header').outerHTML = data;
 	})
 	.catch(error => console.error('머릿말 로드 실패:', error));
 
-fetch('/footer.html')
+fetch('/etc/archive/2025/main/footer.html')
     .then(response => response.text())
     .then(data => {
         const footerElement = document.getElementById('footer');
